0f89c317c3a72f8c0901d6a73c847bba044a9f1c
Clombec <christophe.lombardobeclin@gmail.com>
2024-06-03 21:54:52 +0200

    release .exe with no console

--- Tools/Release.bat
+++ Tools/Release.bat
@@ -1,5 +1,7 @@
 cd ../code 
-pyinstaller -F pdfmergeGUI.py
+pyinstaller --noconsole -F pdfmergeGUI.py
+rem this command may generate a windows virus error
+rem see https://stackoverflow.com/questions/77346372/pyinstaller-says-i-made-a-virus
 
 cd dist
 xcopy pdfmergeGUI.exe Z:\Lombec\Maison\Scanner /Y
